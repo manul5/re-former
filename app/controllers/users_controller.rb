4905c13edd827d4c1ec40ccc6f3c63241b1b8077
manul <manuuva5@gmail.com>
2025-05-28 00:41:22 -0300

    add form whith user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   # app/controllers/users_controller.rb
   def create
-    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
 
     if @user.save
       redirect_to new_users_path
@@ -12,5 +12,10 @@ class UsersController < ApplicationController
 
 
   def new
+  end
+
+  private
+  def user_params
+    params.expect(user: [ :username, :email, :password ])
   end
 end
