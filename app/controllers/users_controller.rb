6a3457211754672f27ce54d3275f7f042e242065
manul <manuuva5@gmail.com>
2025-05-28 10:34:47 -0300

    add edit and update views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,21 @@ class UsersController < ApplicationController
 
 
   def new
+    @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
