93b84ed2590e3abc47e5a73f2edf2a98e4ee20b1
manul <manuuva5@gmail.com>
2025-05-28 00:23:01 -0300

    add user form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,15 @@
 class UsersController < ApplicationController
+  # app/controllers/users_controller.rb
   def create
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+
+    if @user.save
+      redirect_to new_users_path
+    else
+    render :new, status: :unprocessable_entity
+    end
   end
+
 
   def new
   end
